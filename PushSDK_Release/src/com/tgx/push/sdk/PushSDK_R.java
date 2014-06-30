ce802a6dc320e8403092009b83306d276cf616ae
zwh <zwh@newgamer.com>
2014-06-30 12:23:36 +0800

    add project

---
+++ PushSDK_Release/src/com/tgx/push/sdk/PushSDK_R.java
@@ -0,0 +1,46 @@
+package com.tgx.push.sdk;
+
+import android.content.res.Resources;
+
+
+public final class PushSDK_R
+{
+	public static Resources res;
+	
+	public static final class string
+	{
+
+		public static int action_opensdk_consult() {
+			return R.string.action_opensdk_consult;
+		}
+		
+		public static int sdk_domain() {
+			return R.string.sdk_domain;
+		}
+		
+		public static int sdk_scheme() {
+			return R.string.sdk_scheme;
+		}
+		
+		public static int permission_vote() {
+			return R.string.permission_vote;
+		}
+		
+		public static int action_protect_service_start() {
+			return R.string.action_protect_service_start;
+		}
+		
+		public static int permission_service() {
+			return R.string.permission_service;
+		}
+		
+		public static int permission_client() {
+			return R.string.permission_client;
+		}
+		
+		public static int action_opensdk_vote() {
+			return R.string.action_opensdk_vote;
+		}
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 PushSdkBuild
 ============
+
+PushSDK_Release
+----------
+用于生成配置文件的工程
+		
+		pushsdk_thirdpary_string.xml文件
+		
+pushsdk.conf 	文件为默认配置，可以在此基础上修改。当文件不存时，可以通过ant 配置，生成新的配置文件。
+
+build.xml 		配置文件生成脚本。
+
